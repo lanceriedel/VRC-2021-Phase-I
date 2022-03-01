3992f3e4441adc8e2c89f64cb0d93a0700f52239
Lance Riedel <lanceriedel@gmail.com>
2022-02-28 19:16:13 -0600

    reverting out thermal readings

--- PCC/MCU-Software/libraries/Serial Parser/serial_lib.hpp
+++ PCC/MCU-Software/libraries/Serial Parser/serial_lib.hpp
@@ -22,6 +22,12 @@ typedef enum
     SET_SERVO_PCT,
     SET_BASE_COLOR,
     SET_TEMP_COLOR,
+    SET_PIXEL_CYCLE,
+    SET_TRIGGER_SWITCH,
+    SET_SWITCH_ON,
+    SET_SWITCH_OFF,
+    SET_LASER_ON,
+    SET_LASER_OFF,
     RESET_VRC_PERIPH,
     CHECK_SERVO_CONTROLLER,
     COMMAND_END
@@ -34,14 +40,23 @@ typedef struct
   uint8_t data[128];
 } packet_t;
 
+typedef struct 
+{
+  uint8_t command;
+  uint8_t data[2048];
+} packet_send_t;
+
 typedef enum
 {
     QUEUE_EMPTY,
     SUCCESS,
+    DROPPED
 } cmd_result;
 
 static char* outgoing_preamble = "$P>";//towards arduino
 static char* incoming_preamble = "$P<";//towards jetson
+
+
 
 class VRCSerialParser 
 {
@@ -50,7 +65,12 @@ class VRCSerialParser
     void poll(void);
     uart_states get_state(void);
     cmd_result get_command(packet_t* msg);
+    cmd_result set_command(packet_send_t* msg);
+    void construct_payload(uint8_t* data_send_bytes_, int codecommand, int sizedata, uint8_t* data);
+
     uint32_t available = 0;
+    uint32_t available_send = 0;
+
   private:
     Adafruit_USBD_CDC serial_bus;  //arduino serial object
     uart_states state;
@@ -63,13 +83,24 @@ class VRCSerialParser
     uint16_t write_index = 0;
     uint8_t crc_bytes[2] = {0};
     uint8_t messages_available = 0;
+
+    //Space for sending sm to work with
+    char data_send_bytes[4096] = {0};
+
     
     uint8_t crc8_dvb_s2(uint8_t crc, unsigned char a);
     uint8_t calc_crc(uint8_t* buffer, uint16_t length);
 
     uint32_t messages_dropped=0;
+    uint32_t messages_send_dropped=0;
+    cmd_result send_messages();
+    cmd_result read_messages();
+
+
 
     cppQueue q;
+    cppQueue q_send;
+
 
     
 };
